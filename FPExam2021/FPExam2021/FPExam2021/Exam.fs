19a9444d178656e975fd4f418df83b8effc43fbf
Lucasfth <luha@itu.dk>
2023-06-01 13:20:37 +0200

    Finally implement funtion path

--- FPExam2021/FPExam2021/FPExam2021/Exam.fs
+++ FPExam2021/FPExam2021/FPExam2021/Exam.fs
@@ -24,32 +24,72 @@
 
 (* Question 1.1 *)
 
-    type direction = North | East | South | West
-    type coord     = C of int * int
+type direction =
+    | North
+    | East
+    | South
+    | West
 
-    let move _ = failwith "not implemented"
-    let turnRight _ = failwith "not implemented"
-    let turnLeft _ = failwith "not implemented"
-    
+type coord = C of int * int
+
+let move dist (dir: direction) (C(x, y)) =
+    match dir with
+    | North -> C(x, y - dist)
+    | South -> C(x, y + dist)
+    | West -> C(x - dist, y)
+    | East -> C(x + dist, y)
+
+let turnRight dir =
+    match dir with
+    | North -> East
+    | East -> South
+    | South -> West
+    | West -> North
+
+let turnLeft dir = turnRight dir |> turnRight |> turnRight
+
 (* Question 1.2 *)
 
-    type position = P of (coord * direction)
-    type move     = TurnLeft | TurnRight | Forward of int
+type position = P of (coord * direction)
 
-    let step _ = failwith "not implemented"
+type move =
+    | TurnLeft
+    | TurnRight
+    | Forward of int
+
+let step (P(C(x, y), dir)) m =
+    match m with
+    | TurnLeft -> P(C(x, y), turnLeft dir)
+    | TurnRight -> P(C(x, y), turnRight dir)
+    | Forward dis -> P(move dis dir (C(x, y)), dir)
 
 (* Question 1.3 *)
 
+let rec walk pos ms =
+    match ms with
+    | move :: tail -> walk (step pos move) tail
+    | _ -> pos
 
-    let walk _ = failwith "not implemented"
+let walk2 pos ms =
+    List.fold (fun pos m -> step pos m) pos ms
 
 (* Question 1.4 *)
 
-    let path _ = failwith "not implemented"
+let rec path (pos: position) (moves: move list) : coord list =
+    match moves with
+    | [] -> [ getCoord pos ]
+    | move :: tail ->
+        let nextPos = step pos move
+
+        match move with
+        | Forward _ -> getCoord pos :: path nextPos tail
+        | _ -> path nextPos tail
+
+and getCoord (P(coord, _)) = coord
 
 (* Question 1.5 *)
 
-    let path2 _ = failwith "not implemented"
+let path2 _ = failwith "not implemented"
 
 (* Question 1.6 *)
 
@@ -64,30 +104,32 @@
    A: <Your answer goes here>
 *)
 
-    let path3 _ = failwith "not implemented"
+let path3 _ = failwith "not implemented"
 
 (* 2: Code Comprehension *)
-    let foo f =
-        let mutable m = Map.empty
-        let aux x =
-            match Map.tryFind x m with
-            | Some y when Map.containsKey x m -> y
-            | None   -> 
-            m <- Map.add x (f x) m; f x
+let foo f =
+    let mutable m = Map.empty
 
-        aux
+    let aux x =
+        match Map.tryFind x m with
+        | Some y when Map.containsKey x m -> y
+        | None ->
+            m <- Map.add x (f x) m
+            f x
 
-    let rec bar x =
-      match x with 
-      | 0 -> 0 
-      | 1 -> 1
-      | y -> baz (y - 1) + baz (y - 2)
+    aux
 
-    and baz = foo bar
+let rec bar x =
+    match x with
+    | 0 -> 0
+    | 1 -> 1
+    | y -> baz (y - 1) + baz (y - 2)
+
+and baz = foo bar
 
 (* Question 2.1 *)
 
-    (* 
+(* 
     
     Q: What are the types of functions foo, bar, and baz?
 
@@ -117,12 +159,12 @@
     A: <Your answer goes here>
     
     *)
-        
+
 
 (* Question 2.2 *)
 
- 
-    (* 
+
+(* 
     The code includes the keyword "and".
 
     
@@ -140,9 +182,9 @@
 
     *)
 
-(* Question 2.3 *) 
+(* Question 2.3 *)
 
-    (* 
+(* 
     The function foo generates a warning during compilation:
     "Warning: Incomplete pattern matches on this expression.".
 
@@ -165,18 +207,19 @@
 
     *)
 
-    let foo2 _ = failwith "not implemented"
+let foo2 _ = failwith "not implemented"
 
 (* Question 2.4 *)
 
-    let rec barbaz x =
-        let baz = foo barbaz
-        match x with 
-        | 0 -> 0 
-        | 1 -> 1
-        | y -> baz (y - 1) + baz (y - 2)
+let rec barbaz x =
+    let baz = foo barbaz
 
-    (*
+    match x with
+    | 0 -> 0
+    | 1 -> 1
+    | y -> baz (y - 1) + baz (y - 2)
+
+(*
 
     Q: Without explicitly timing the execution times, compare the execution
        times of baz and barbaz. One is slower than the other.
@@ -188,29 +231,29 @@
     *)
 (* Question 2.5 *)
 
-    let bazSeq _ = failwith "not implemented"
+let bazSeq _ = failwith "not implemented"
 
 (* 3: Guess the next sequence element *)
 
 (* Question 3.1 *)
 
-    type element = unit (* Your type goes here in stead of unit *)
+type element = unit (* Your type goes here in stead of unit *)
 
 (* Question 3.2 *)
 
-    let elToString _ = failwith "not implemented"
-    let elFromString _ = failwith "not implemented"
+let elToString _ = failwith "not implemented"
+let elFromString _ = failwith "not implemented"
 
 (* Question 3.3 *)
 
-    let nextElement _ = failwith "not implemented"
+let nextElement _ = failwith "not implemented"
 
 (* Question 3.4 *)
 
-    let elSeq _ = failwith "not implemented"
-    let elSeq2 _ = failwith "not implemented"
+let elSeq _ = failwith "not implemented"
+let elSeq2 _ = failwith "not implemented"
 
-    (*
+(*
 
     Q: Why would Seq.initInfinite not be an appropriate choice to
        write a function like elSeq?
@@ -221,70 +264,71 @@
 
 (* Question 3.5 *)
 
-    let compress _ = failwith "not implemented"
+let compress _ = failwith "not implemented"
 
 (* Question 3.6 *)
 
-    let elParse _ = failwith "not implemented"
-    let elFromString2 _ = failwith "not implemented"
+let elParse _ = failwith "not implemented"
+let elFromString2 _ = failwith "not implemented"
 
 (* 4: Rings *)
 
 (* Question 4.1 *)
 
-    type 'a ring = RemoveThisConstructor of 'a (* replace this entire type with your own *)
+type 'a ring = RemoveThisConstructor of 'a (* replace this entire type with your own *)
 
 (* Question 4.2 *)
 
-    let length _ = failwith "not implemented"
-    let ringFromList _ = failwith "not implemented"
-    let ringToList _ = failwith "not implemented"
+let length _ = failwith "not implemented"
+let ringFromList _ = failwith "not implemented"
+let ringToList _ = failwith "not implemented"
 
 (* Question 4.3 *)
 
-    let empty _ = failwith "not implemented"
-    let push _ = failwith "not implemented"
-    let peek _ = failwith "not implemented"
-    let pop _ = failwith "not implemented"
-    let cw _ = failwith "not implemented"
-    let ccw _ = failwith "not implemented"
+let empty _ = failwith "not implemented"
+let push _ = failwith "not implemented"
+let peek _ = failwith "not implemented"
+let pop _ = failwith "not implemented"
+let cw _ = failwith "not implemented"
+let ccw _ = failwith "not implemented"
 
 (* Question 4.4 *)
 
-    type StateMonad<'a, 'b> = SM of ('b ring -> ('a * 'b ring) option)
-    let ret x = SM (fun st -> Some (x, st))
-    let bind (SM m) f =
-        SM (fun st ->
-            match m st with
-            | None -> None
-            | Some (x, st') ->
-                let (SM g) = f x
-                g st')
+type StateMonad<'a, 'b> = SM of ('b ring -> ('a * 'b ring) option)
+let ret x = SM(fun st -> Some(x, st))
 
-    let (>>=) m f = bind m f
-    let (>>>=) m n = m >>= (fun () -> n)
-    let evalSM (SM f) s = f s
+let bind (SM m) f =
+    SM(fun st ->
+        match m st with
+        | None -> None
+        | Some(x, st') ->
+            let (SM g) = f x
+            g st')
 
-    let smLength _ = failwith "not implemented"
-    let smPush _ = failwith "not implemented"
-    let smPop _ = failwith "not implemented"
-    let smCW _ = failwith "not implemented"
-    let smCCW _ = failwith "not implemented"
+let (>>=) m f = bind m f
+let (>>>=) m n = m >>= (fun () -> n)
+let evalSM (SM f) s = f s
+
+let smLength _ = failwith "not implemented"
+let smPush _ = failwith "not implemented"
+let smPop _ = failwith "not implemented"
+let smCW _ = failwith "not implemented"
+let smCCW _ = failwith "not implemented"
 
 (* Question 4.4 *)
 
-    (* You may solve this exercise either using monadic operators or 
+(* You may solve this exercise either using monadic operators or 
         using computational expressions. *)
 
-    type StateBuilder() =
+type StateBuilder() =
 
-        member this.Bind(x, f)    = bind x f
-        member this.Zero ()       = ret ()
-        member this.Return(x)     = ret x
-        member this.ReturnFrom(x) = x
-        member this.Combine(a, b) = a >>= (fun _ -> b)
+    member this.Bind(x, f) = bind x f
+    member this.Zero() = ret ()
+    member this.Return(x) = ret x
+    member this.ReturnFrom(x) = x
+    member this.Combine(a, b) = a >>= (fun _ -> b)
 
-    let state = new StateBuilder()
+let state = new StateBuilder()
 
-    let ringStep _ = failwith "not implemented"
-    let iterRemoveSumEven _ = failwith "not implemented"
+let ringStep _ = failwith "not implemented"
+let iterRemoveSumEven _ = failwith "not implemented"
---
+++ FPExam2021/FPExam2021/FPExam2021/obj/Debug/net5.0/.NETCoreApp,Version=v5.0.AssemblyAttributes.fs
@@ -0,0 +1,4 @@
+namespace Microsoft.BuildSettings
+
+[<System.Runtime.Versioning.TargetFrameworkAttribute(".NETCoreApp,Version=v5.0", FrameworkDisplayName = ".NET 7.0")>]
+do ()
